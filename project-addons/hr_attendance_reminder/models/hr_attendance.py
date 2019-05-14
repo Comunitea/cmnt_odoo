c9579d7762b3e8ec46f7cf6af1212d49f2db83e7
JesusVMayor <jesus@comunitea.com>
2019-05-14 16:11:34 +0200

    [IMP]hr_attendance_reminder: Solo enviamos correos hasta 1 hora después de la fecha de entrada/salida, para evitar enviar gran cantidad de correos.

--- project-addons/hr_attendance_reminder/models/hr_attendance.py
+++ project-addons/hr_attendance_reminder/models/hr_attendance.py
@@ -10,10 +10,23 @@ class HrAttendance(models.Model):
 
     _inherit = 'hr.attendance'
 
+    def get_nearest_interval(self, intervals):
+        closest_time = False
+        closest_interval = False
+        for interval in intervals:
+            if interval[1] > datetime.now():
+                raise Exception()
+            if not closest_time or (datetime.now() - interval[1]).seconds \
+                    < closest_time:
+                closest_time = (datetime.now() - interval[1]).seconds < \
+                    closest_time
+                closest_interval = interval
+        return closest_interval
+
     @api.model
     def cron_attendance_reminder(self):
-        for employee in self.env['hr.employee'].search([('calendar_id', '!=', False)]):
-
+        for employee in self.env['hr.employee'].search(
+                [('calendar_id', '!=', False)]):
             currently_working = employee.state == 'present' and True or False
             calendar = employee.calendar_id
             intervals = calendar.get_working_intervals_of_day(
@@ -21,18 +34,26 @@ class HrAttendance(models.Model):
             if intervals:
                 intervals = intervals[0]
             if currently_working:
-                should_be_working = False
-                for interval in intervals:
-                    if interval[1] > datetime.now() + timedelta(minutes=-calendar.reminder_delay):
-                        should_be_working = True
-                if not should_be_working:
-                    self.env.ref('hr_attendance_reminder.email_template_attendance_reminder').send_mail(employee.id)
-                    break
+                try:
+                    nearest_interval = self.get_nearest_interval(intervals)
+                except Exception:
+                    # Aun está en su horario por lo que pasamos al siguiente.
+                    continue
+                if (datetime.now() - nearest_interval[1]).seconds \
+                        / 60.0 / 60.0 > 1:
+                    # Aunque ya se haya pasado el momento de salida,
+                    # si ya ha pasado mas de 1 hora no se envia,
+                    # para evitar el envio continuo de emails.
+                    continue
+                self.env.ref('hr_attendance_reminder.email_template_attendance_reminder').send_mail(employee.id)
             else:
                 for interval in intervals:
                     if interval[1] < datetime.now() or \
                                 interval[0] > datetime.now():
                         continue
-                    if interval[0] < datetime.now() + timedelta(minutes=-calendar.reminder_delay):
+                    if interval[0] < datetime.now() + \
+                            timedelta(minutes=-calendar.reminder_delay) and \
+                            (datetime.now() - interval[0]).seconds\
+                            / 60.0 / 60.0 < 1:
                         self.env.ref('hr_attendance_reminder.email_template_attendance_reminder').send_mail(employee.id)
                         break

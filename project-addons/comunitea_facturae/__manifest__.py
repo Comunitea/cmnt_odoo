629aff0d051684d675b970d80d8aee7a2fdf30c8
JesusVMayor <jesus@comunitea.com>
2022-07-14 18:08:30 +0200

    modulo con nuevos campos facturae

---
+++ project-addons/comunitea_facturae/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2022 Comunitea
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': 'Comunitea facturae',
+    'version': '10.0.1.0.0',
+    'summary': 'Añade campos en factura electronica',
+    'license': 'AGPL-3',
+    'depends': [
+        'account',
+        'l10n_es_facturae'
+    ],
+    'data': [
+        'account_invoice_view.xml',
+    ]
+}
